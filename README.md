5cb5015bbb3130237461503c4bdd9f716cc26160
cloudnost <rich@cloudnost.com>
2025-02-24 12:54:25 -0500

    setting up ecs fargate config

---
+++ README.md
@@ -0,0 +1,1 @@
+# Jenkins-Fargate
--- main.tf
+++ main.tf
@@ -289,6 +289,54 @@ resource "aws_ecs_cluster" "jenkins_fargate_cluster" {
   name = "jenkins-fargate-cluster"
 }
 
+resource "aws_iam_role" "ecs_task_role" {
+  name = "jenkins-ecs-agent-role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+#resource "aws_iam_role_policy_attachment" "ecs_task_role_policy" {
+#  role       = aws_iam_role.ecs_task_role.name
+#  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+#}
+
+resource "aws_iam_policy_attachment" "ecs_task_role_policy_attachment" {
+  name       = "ecs-task-role-policy-attachment"
+  roles      = [aws_iam_role.ecs_task_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_task_definition" "jenkins_agent" {
+  family                = "jenkins-agent"
+  requires_compatibilities = ["FARGATE"]
+  network_mode          = "awsvpc"
+  cpu                   = "256"
+  memory                = "512"
+  execution_role_arn    = aws_iam_role.ecs_task_role.arn
+  task_role_arn         = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([{
+    name      = "jenkins-agent"
+    image     = "jenkins/inbound-agent:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 8080
+      protocol      = "tcp"
+    }]
+  }])
+}
+
 # Output Jenkins URL
 output "jenkins_url" {
   value = "http://${aws_instance.jenkins_master.public_ip}:8080"
